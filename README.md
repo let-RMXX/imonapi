fd08836fe9947d316a3cf0fa1cca0bc7909faa4b
Rui Machado <102834545+let-RMXX@users.noreply.github.com>
2024-02-05 00:31:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# imonapi
+ 
---
+++ resources/views/users/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+ 
+<head>
+
+    <title>I'M ONLINE API - CRUD APP</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="container">
+
+    @yield('content')
+
+</div>
+
+</body>
+
+</html>
